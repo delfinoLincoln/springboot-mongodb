aaf4cd1ab8f4e9133eb01e7910ba194136abc01a
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-05 18:18:36 -0300

    User - FindById method and ObjectNotFoundException created

--- src/main/java/com/delfinolincoln/coursemongo/repositories/UserRepository.java
+++ src/main/java/com/delfinolincoln/coursemongo/repositories/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.delfinolincoln.coursemongo.entities.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
     
 }
