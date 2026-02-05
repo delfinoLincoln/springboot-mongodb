2d4e421c45e90d3c6adb894788c368697a84327d
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-05 16:25:24 -0300

    first commit

---
+++ src/main/java/com/delfinolincoln/coursemongo/CoursemongoApplication.java
@@ -0,0 +1,12 @@
+package com.delfinolincoln.coursemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemongoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/delfinolincoln/coursemongo/CoursemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfinolincoln.coursemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
