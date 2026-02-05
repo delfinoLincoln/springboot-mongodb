c2f47fca34ee9c6e1ab6a37400c9f9e645fc1eab
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-05 17:34:04 -0300

    Entity User, Rest Controller, repository and service created and mongodb connected

---
+++ src/main/java/com/delfinolincoln/coursemongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.delfinolincoln.coursemongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.delfinolincoln.coursemongo.entities.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+    
+}
