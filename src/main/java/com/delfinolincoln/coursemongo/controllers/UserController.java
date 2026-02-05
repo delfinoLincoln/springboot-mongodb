b5af04b27cb1c9f13d448fa0d7da5ab9f0be19d9
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-05 17:57:10 -0300

    Standard DTO to return user

--- src/main/java/com/delfinolincoln/coursemongo/controllers/UserController.java
+++ src/main/java/com/delfinolincoln/coursemongo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.delfinolincoln.coursemongo.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.delfinolincoln.coursemongo.dto.UserDto;
 import com.delfinolincoln.coursemongo.entities.User;
 import com.delfinolincoln.coursemongo.services.UserService;
 
@@ -19,8 +21,9 @@ public class UserController {
     private UserService userService;
     
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
