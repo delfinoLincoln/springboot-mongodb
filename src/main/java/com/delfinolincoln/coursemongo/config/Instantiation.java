b54ad0c5c291f5690f36d099534f247dbc00c23e
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-06 17:45:05 -0300

    Reference user posts in User entity

--- src/main/java/com/delfinolincoln/coursemongo/config/Instantiation.java
+++ src/main/java/com/delfinolincoln/coursemongo/config/Instantiation.java
@@ -39,10 +39,12 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Leo Jardim", "jardim@gmail.com"); 
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
-        
+
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDto(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+        maria.getPost().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
